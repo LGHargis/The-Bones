24446192cfbb5e140747ea1fae3258a7bd022f7f
felsuna <felicia.sunanto@gmail.com>
2019-02-19 14:21:53 -0700

    commented out a code that is throwing an error for now

--- models/Translate.js
+++ models/Translate.js
@@ -15,15 +15,17 @@ module.exports = function (sequelize, DataTypes) {
     }
   });
 
+  // ** commented out for now, it's throwing an error that UserId cannot be null with sequelize **
+
   // Add a belongsTo association to Users here
-  Translate.associate = function (models) {
-    //  a translate should belong to a User
-    // A translate can't be created without a user due to the foreign key constraint
-    Translate.belongsTo(models.User, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
+  // Translate.associate = function (models) {
+  //   //  a translate should belong to a User
+  //   // A translate can't be created without a user due to the foreign key constraint
+  //   Translate.belongsTo(models.User, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
   return Translate;
 };
